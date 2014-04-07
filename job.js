1c73442530c9ab825068d732cd6d19a81b241cc8
Aleksis Brezas <abresas@gmail.com>
2014-04-07 23:37:00 +0300

    remove unnessary comments

--- job.js
+++ job.js
@@ -2,7 +2,6 @@ var fs = require( 'fs' );
 
 var arg = process.argv[ 2 ];
 
-
 function functionReviver(key, value) {
     if (key === '') {
         return value;
@@ -22,7 +21,6 @@ function functionReviver(key, value) {
     }
     return value;
 }
-
 
 var msg = JSON.parse( arg, functionReviver );
 
@@ -45,7 +43,6 @@ process.stdin.on( 'readable', function() {
     var messages = d.toString('ascii').split( '\n' );
     var i = 0;
     while ( i < messages.length ) {
-        // message = messages[ i ].trim();
         message = messages[ i ];
         if (i === messages.length - 1 ) {
             d = message;
@@ -53,37 +50,24 @@ process.stdin.on( 'readable', function() {
             continue;
         }
         if ( !message.length ) {
-            //console.error( "no length", message, +message, script( +message ) );
             ++i;
             continue;
         }
         _data = +message; // TODO: other types
         var _result;
-        // context[ '_data' ] = parseFloat( message );
         if ( _data ) {
             _result = script( _data );
         }
         // var _result = script.runInNewContext( context );
         // console.error( 'process', process.pid, JSON.stringify( message ), _result );
         if ( _result ) {
-            // outbuf.concat( _result + '\n' );
-            // outbuf = Buffer.concat( [ outbuf, new Buffer( _result + '\n', 'ascii' ) ] );
             outbuf += _result + '\n';
             if ( outbuf.length > 100 || ending ) {
-                // if ( ending ) { console.error( 'ending' ); }
-                /*
-                var b = fs.writeSync( 1, new Buffer( outbuf, 'ascii' ), 0, outbuf.length, null, function(){} );
-                if ( b != outbuf.length ) {
-                    console.error( 'error writing on job', process.pid, b.length, outbuf.length );
-                }
-                */
                 var b = process.stdout.write( outbuf, 'ascii' );
                 if ( b ) {
                     outbuf = '';
                 }
             }
-            //process.stdout.write( _result + '\n' );
-            // console.log( _result );
         }
         ++i;
     }
@@ -93,10 +77,8 @@ process.stdin.on( 'readable', function() {
 process.stdin.on( 'end', function() {
     ending = true;
     // console.error( 'data on end', outbuf.length, outbuf );
-    // fs.writeSync( 1, new Buffer( outbuf, 'ascii' ), 0, outbuf.length, null );
     process.stdout.write( outbuf, 'ascii' );
     outbuf = '';
     // process.exit();
 } );
 // console.error( 'process:', process.pid, 'job:', script, 'context: ', context );
-
