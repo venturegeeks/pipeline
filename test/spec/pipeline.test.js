d3d79a92877c0649293bb02ec562722152d87ae4
Thanasis Polychronakis <thanpolas@gmail.com>
2014-04-07 20:11:58 +0300

    expose reliability bug in test case

--- test/spec/pipeline.test.js
+++ test/spec/pipeline.test.js
@@ -4,7 +4,24 @@
 var chai = require('chai');
 var expect = chai.expect;
 
+var Pipeline = require( '../..' );
+
+
 describe('API Surface', function() {
-  it('Will ...', function() {
+  it('Will perform reliably', function(done) {
+    var invocations = 0;
+    for (var i = 0; i < 10; i++) {
+      Pipeline.range( 1, 10000 ).filter( function( x ) {
+          return ( x % 3 === 0 || x % 5 === 0 );
+      } ).reduce( 0, function( x, sum ) {
+          return x + sum;
+      } ).on( 'complete', function( data ) {
+          var total = data[ data.length - 1 ];
+          expect(total).to.equal(23331668);
+          if (++invocations === 10) {
+            done();
+          }
+      } );
+    }
   });
 });
