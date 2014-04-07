9958f0537c93a80fdd7eb62aaf95839222cb45d2
Aleksis Brezas <abresas@gmail.com>
2014-04-08 01:01:31 +0300

    add word count example

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ var Pipeline = require( './pipeline' );
 
 var t1 = process.hrtime();
 
-Pipeline.range( 1, 10000 ).filter( function( x ) {
+Pipeline.range( 1, 1000 ).filter( function( x ) {
     return ( x % 3 === 0 || x % 5 === 0 );
 } ).reduce( 0, function( x, sum ) {
     return x + sum;
@@ -12,8 +12,15 @@ Pipeline.range( 1, 10000 ).filter( function( x ) {
     console.log( 'time', tdiff[ 0 ] * 1000 + tdiff[ 1 ] / 1000000, 'ms' );
 } );
 
-/*
-Pipeline.range( 1, 1000000 ).on( 'complete', function() {
-    console.log( 'range' );
+Pipeline.range( 1, 10 ).map( function( x ) {
+    return "x=" + x;
+}, { outputFormat: 'string' } ).on( 'complete', function( data ) {
+    console.log( data );
 } );
-*/
+
+Pipeline.readFile( './LICENSE' ).reduce( 0, function( line, sofar ) {
+    return sofar + line.match( /\S+/g ).length;
+} ).on( 'complete', function( data ) {
+    var total = data[ data.length - 1 ];
+    console.log( 'license word count', total );
+} );
